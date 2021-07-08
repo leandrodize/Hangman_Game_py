d83674c04e5076d808a49aee2ed927972d9e116a
Leonidas111 <juanca930.jcdz@gmail.com>
2021-07-08 18:13:45 -0500

    MENU BRANCH: 5 OPTIONS IN THE MENU

--- hangmangame.py
+++ hangmangame.py
@@ -1,3 +1,4 @@
+import os
 import pyfiglet
 
 
@@ -8,10 +9,37 @@ def run():
     1)START GAME
     2)INSTRUCTIONS
     3)SEE WORDS
-    4)ADD WORDS
+    4)ADD WORD
     5)EXIT GAME
     WRITE HERE THE OPTION NUMBER ==> """
-    
+    option = int(input(menu))
+    if option == 1:
+        os.system("cls")
+        print(fihangman)
+        print("""
+                     ___
+                    |   |
+                   _O/  |
+                    |   |
+                   / \  |
+                        |
+                   ========== """)
+    if option == 2:
+        os.system("cls")
+        fiinstructions = pyfiglet.figlet_format("INSTRUCTIONS")
+        print(fiinstructions)
+    if option == 3:
+        os.system("cls")
+        fiseewords = pyfiglet.figlet_format("LIST WORDS:")
+        print(fiseewords)
+    if option == 4:
+        os.system("cls")
+        fiaddwords = pyfiglet.figlet_format("ADD WORD")
+
+
+
+
+
 
 
 if __name__=="__main__":
